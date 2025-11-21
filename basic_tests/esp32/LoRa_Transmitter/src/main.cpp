7dff4b4f2f767e88f044c8dd0b16ace6cb8899fb
rprecigapuentes <rprecigapuentes@gmail.com>
2025-11-20 22:43:57 -0500

    ejemplos básicos - se confirmó correcto funcionamiento de los 4 LoRa, esp32 y esp32 mini

---
+++ basic_tests/esp32/LoRa_Transmitter/src/main.cpp
@@ -0,0 +1,130 @@
+/*******************************************************************************************************
+  Programs for Arduino - Copyright of the author Stuart Robinson - 02/03/20
+
+  This program is supplied as is, it is up to the user of the program to decide if the program is
+  suitable for the intended purpose and free from errors.
+*******************************************************************************************************/
+
+
+/*******************************************************************************************************
+  Program Operation  - This is a minimum setup LoRa test transmitter. A packet containing the ASCII text
+  "Hello World 1234567890" is sent using the frequency and LoRa settings specified in the LT.setupLoRa()
+  command. The pins to access the LoRa device need to be defined at the top of the program also.
+
+  The details of the packet sent and any errors are shown on the Arduino IDE Serial Monitor, together with
+  the transmit power used and the packet length. The matching receiver program, '4_LoRa_Receiver' can be used
+  to check the packets are being sent correctly, the frequency and LoRa settings (in the LT.setupLoRa()
+  commands) must be the same for the transmitter and receiver programs. Sample Serial Monitor output;
+
+  10dBm Packet> Hello World 1234567890*  BytesSent,23  PacketsSent,6
+
+  For an example of a more detailed configuration for a transmitter, see program 103_LoRa_Transmitter.
+
+  Serial monitor baud rate is set at 115200
+*******************************************************************************************************/
+
+#include <SPI.h>                                //the lora device is SPI based so load the SPI library                                         
+#include <SX127XLT.h>                           //include the appropriate library  
+
+SX127XLT LT;                                    //create a library class instance called LT
+
+//ESP32 LoRa pins (adjust to your wiring)
+#define LORA_SCK   18                           //SCK on ESP32 (VSPI SCK)
+#define LORA_MISO  19                           //MISO on ESP32 (VSPI MISO)
+#define LORA_MOSI  23                           //MOSI on ESP32 (VSPI MOSI)
+
+#define NSS        5                            //select pin on LoRa device (CS)
+#define NRESET     14                           //reset pin on LoRa device
+#define DIO0       2                            //DIO0 pin on LoRa device, used for sensing RX and TX done 
+
+#define LORA_DEVICE DEVICE_SX1278               //we need to define the device we are using
+#define TXpower 10                              //LoRa transmit power in dBm
+
+uint8_t TXPacketL;
+uint32_t TXPacketCount;
+
+uint8_t buff[] = "Hello World 1234567890";      //the message to send
+
+//function prototypes
+void packet_is_OK();
+void packet_is_Error();
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(F("3_LoRa_Transmitter Starting"));
+
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, NSS);   //initialise SPI with explicit pins for ESP32
+
+  if (LT.begin(NSS, NRESET, DIO0, LORA_DEVICE))
+  {
+    Serial.println(F("LoRa Device found"));
+    delay(1000);
+  }
+  else
+  {
+    Serial.println(F("No device responding"));
+    while (1)
+    {
+      delay(2000);                                  //avoid watchdog reset on ESP32
+    }
+  }
+
+  LT.setupLoRa(434000000, 0, LORA_SF7, LORA_BW_125, LORA_CR_4_5, LDRO_AUTO); //configure frequency and LoRa settings
+
+  Serial.print(F("Transmitter ready"));
+  Serial.println();
+}
+
+
+void loop()
+{
+  Serial.print(TXpower);                                       //print the transmit power defined
+  Serial.print(F("dBm "));
+  Serial.print(F("Packet> "));
+  Serial.flush();
+
+  TXPacketL = sizeof(buff);                                    //set TXPacketL to length of array
+  buff[TXPacketL - 1] = '*';                                   //replace null character at buffer end so its visible on receiver
+
+  LT.printASCIIPacket(buff, TXPacketL);                        //print the buffer (the sent packet) as ASCII
+
+  if (LT.transmit(buff, TXPacketL, 10000, TXpower, WAIT_TX))   //will return packet length sent if OK, otherwise 0 if transmit error
+  {
+    TXPacketCount++;
+    packet_is_OK();
+  }
+  else
+  {
+    packet_is_Error();                                         //transmit packet returned 0, there was an error
+  }
+
+  Serial.println();
+  delay(1000);                                                 //have a delay between packets
+}
+
+
+void packet_is_OK()
+{
+  //if here packet has been sent OK
+  Serial.print(F("  BytesSent,"));
+  Serial.print(TXPacketL);                             //print transmitted packet length
+  Serial.print(F("  PacketsSent,"));
+  Serial.print(TXPacketCount);                         //print total of packets sent OK
+}
+
+
+void packet_is_Error()
+{
+  //if here there was an error transmitting packet
+  uint16_t IRQStatus;
+  IRQStatus = LT.readIrqStatus();                      //read the the interrupt register
+  Serial.print(F(" SendError,"));
+  Serial.print(F("Length,"));
+  Serial.print(TXPacketL);                             //print transmitted packet length
+  Serial.print(F(",IRQreg,"));
+  Serial.print(IRQStatus, HEX);                        //print IRQ status
+  LT.printIrqStatus();                                 //prints the text of which IRQs set
+}
